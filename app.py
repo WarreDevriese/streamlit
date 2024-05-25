baef255a5e3abc4f7fb3d02d80c602042df6a6fa
Edouard <128392994+WarreDevriese@users.noreply.github.com>
2024-05-25 08:30:52 +0200

    Initial commig

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello!",
+    page_icon="🤓",
+)
+
+st.write("# Hello world!🤓")
+st.write("## Bananas are either too small or too big for me")
