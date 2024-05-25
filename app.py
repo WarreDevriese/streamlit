c82d958deb8269873b067006aca23916dbaf2fce
Edouard <128392994+WarreDevriese@users.noreply.github.com>
2024-05-25 10:28:57 +0200

    Exploring basic features

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Hello!",
-    page_icon="🤓",
-)
-
-st.write("# Hello world!🤓")
-st.write("## Bananas are either too small or too big for me")
